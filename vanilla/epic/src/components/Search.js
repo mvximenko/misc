ac580a46e495b18b6ce37661e0e128ee186ecadd
mvximenko <mr.mxmnk@gmail.com>
2021-09-28 20:25:08 +0900

    Move epic to its own repository

--- vanilla/epic/src/components/Search.js
+++
@@ -1,9 +0,0 @@
-const Search = () => {
-  const template = `
-    <input type="text" id="search" />
-  `;
-
-  return template;
-};
-
-export default Search;
--- vanilla/epic/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_IMG = 'https://cdn.akamai.steamstatic.com/steam/apps';
-export const API_STORE = 'https://store.steampowered.com/app';
